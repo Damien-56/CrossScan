c5c5947a3f786f9fce07a80d82176f443fef22cd
Damien-56 <guillendamien@gmail.com>
2026-09-23 14:18:55 +0200

    Cache 80-runner burst test for offline use

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const C='crossscan-beta48-offline-v1';
-const A=['./','./index.html','./manifest.webmanifest','./vendor/zxing-browser.min.js','./vendor/xlsx.full.min.js','./vendor/qrcode-generator.min.js','./icons/icon-192.png','./icons/icon-512.png','./icons/icon-maskable-512.png'];
+const C='crossscan-beta410-test-v1';
+const A=['./','./index.html','./manifest.webmanifest','./test-rafale-80.html','./vendor/zxing-browser.min.js','./vendor/xlsx.full.min.js','./vendor/qrcode-generator.min.js','./icons/icon-192.png','./icons/icon-512.png','./icons/icon-maskable-512.png'];
 self.addEventListener('install',e=>e.waitUntil((async()=>{const c=await caches.open(C);await c.addAll(A);self.skipWaiting()})()));
 self.addEventListener('activate',e=>e.waitUntil((async()=>{for(const k of await caches.keys())if(k!==C)await caches.delete(k);await self.clients.claim()})()));
 self.addEventListener('fetch',e=>{
